0d703cd9facf10d63336fc44c5d0d7225ff75e6a
MHamdan07 <entermh07@gamail.com>
2026-08-03 22:48:17 +0500

    fix: import Outlet from react-router-dom in MainLayout.jsx to fix ReferenceError: Outlet is not defined

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import SubscriptionGatekeeper from '../components/SubscriptionGatekeeper';
